10d74db554023be7d3acbc1f497688604666dc73
Antonio Van Grieken <antovang@gmail.com>
2021-11-23 14:51:23 +0100

    fix: polymorphic call in ActivityManager & feat: abstract methods implemented in OrganizationActivityManager

--- Model/Managers/OrganizationActivityManager.php
+++ Model/Managers/OrganizationActivityManager.php
@@ -3,28 +3,48 @@
 namespace Model\Managers;
 
 use Model\Entities\Entity;
+use Model\Entities\OrganizationActivity;
 use PDOStatement;
 
 class OrganizationActivityManager extends PDOManager
 {
     public function findById(int $id): ?Entity
     {
-        // TODO: Implement findById() method.
+        //Préparation du PDOStatement
+        $stmt = $this->executePrepare("select * from secteurs_structures where id=:id", [ 'id' => $id]);
+        $organizationActivity = $stmt->fetch();
+
+        //Si on ne trouve pas l'activité on renvoie NULL
+        if (!$organizationActivity)
+            return null;
+
+        return new OrganizationActivity($organizationActivity['ID'],$organizationActivity['ID_STRUCTURE'],$organizationActivity['ID_SECTEUR']);
     }
 
     public function find(): PDOStatement
     {
-        // TODO: Implement find() method.
+        $stmt = $this->executePrepare("select * from secteur_structures",[]);
+        return $stmt;
     }
 
     public function findAll(int $pdoFecthMode): array
     {
-        // TODO: Implement findAll() method.
+        $stmt = $this->find();
+        $organizationsActivities = $stmt->fetchAll($pdoFecthMode);
+
+        $organization_activity_entities=[];
+        foreach($organizationsActivities as $organizationActivity) {
+            $organization_activity_entities[] = new OrganizationActivity($organizationActivity['ID'],$organizationActivity['ID_STRUCTURE'],$organizationActivity['ID_SECTEUR']);
+        }
+        return $organizationsActivities;
     }
 
     public function insert(Entity $e): PDOStatement
     {
-        // TODO: Implement insert() method.
+        $req = "insert into secteurs_structures(id, idStructure, idSecteur) values (:id, :idStructure, :idSecteur)";
+        $params = array('id' => $e->getId(), 'idStructure' => $e->getOrganizationId(), 'idSecteur' => $e->getActivityId());
+        $res = $this->executePrepare($req,$params);
+        return $res;
     }
 
 }
