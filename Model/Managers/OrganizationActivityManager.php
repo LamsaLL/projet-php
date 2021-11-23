02c926d0467f41d70728ad5a1dfa3f4c087d5df3
Antonio Van Grieken <antovang@gmail.com>
2021-11-23 13:16:17 +0100

    Implement abstract methods in Models extends PDOManager

--- Model/Managers/OrganizationActivityManager.php
+++ Model/Managers/OrganizationActivityManager.php
@@ -2,7 +2,29 @@
 
 namespace Model\Managers;
 
-class OrganizationActivityManager
+use Model\Entities\Entity;
+use PDOStatement;
+
+class OrganizationActivityManager extends PDOManager
 {
+    public function findById(int $id): ?Entity
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function find(): PDOStatement
+    {
+        // TODO: Implement find() method.
+    }
+
+    public function findAll(int $pdoFecthMode): array
+    {
+        // TODO: Implement findAll() method.
+    }
+
+    public function insert(Entity $e): PDOStatement
+    {
+        // TODO: Implement insert() method.
+    }
 
 }
